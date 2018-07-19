7ef49467bc3a17307118bc65ad746b171fe0fb97
Marc Newport <marcnewport@gmail.com>
2018-07-19 11:34:31 +1000

    tidied up css

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+
+  const nav = document.querySelector('nav');
   const audio = document.querySelector('audio');
   const baby = document.querySelector('.baby');
 
   baby.addEventListener('click', () => {
+    nav.classList.toggle('is-showing');
+    audio.currentTime = 0;
     audio.play();
-  })
+  });
 });
